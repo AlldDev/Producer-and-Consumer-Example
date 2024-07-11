5c07d4dd23815bbd047a33795ebfd6f4a88f7e01
Alessandro César Rosão <142243633+AlldDev@users.noreply.github.com>
2024-07-11 18:01:42 -0300

    Update producer-and-consumer.py

--- example-1/producer-and-consumer.py
+++ example-1/producer-and-consumer.py
@@ -1,59 +1,46 @@
 import threading
 import time
 
-class Tela:
+class ProducerAndConsumer:
     def __init__(self):
         self._buffer = []                       # Buffer
         self._buffer_lock = threading.Lock()    # Lock para o Buffer
         self._buffer_event = threading.Event()  # Event para o buffer
 
-        # Status das threads para uma janela (True = sendo executada, False = Parar)
-        self._consumer_status = True
         self._producer_status = True
-
+        self._consumer_status = True
+        
         # Iniciando as threads para rodarem simultaneamente
-        consumer_thread = threading.Thread(target=self.consumer, args=())
-        producer_thread = threading.Thread(target=self.producer, args=(10,))
-
-        # Iniciando as Threads
-        consumer_thread.start()
-        producer_thread.start()
-
-    # Thread para colocar dados no buffer
-    # nesse caso ainda é necessário tratar qual é a mensagem que estamos recebendo
-    # para chamar o buffer da interface certa (mensagem, status, etc)
-    # e essa função possivelmente ficará dentro de APP, pois vai ter apenas ela produzindo
+        self._producer_thread = threading.Thread(target=self.producer, args=(10,))
+        self._consumer_thread = threading.Thread(target=self.consumer, args=())
+        
     def producer(self, delay):
-        # Enquanto estivermos executando
         while self._producer_status:
-            # Simula a demora entre as mensagens recebida
             time.sleep(delay)
 
             with self._buffer_lock:
                 self._buffer.append('Hello World')
+                print(f'Coloquei no Buffer: {self._buffer[-1]}')
+                # Subindo o evento para a consumer saber
                 self._buffer_event.set()
 
-    # Thread para retirar os dados de dentro do Buffer
-    # terá uma dessa para cada janela, onde a producer
-    # irá tratar e subir o event para a janela correta
     def consumer(self):
-        # Enquanto estivermos executando
         while self._consumer_status:
-            # Aqui a mágica acontece, chamando essa linha, a thread vai ficar
-            # dormindo até ter algo pra ela tratar, que é quando a producer
-            # subir o event
+            # Aguarda até a producer subir o evento
             self._buffer_event.wait()
 
-            # Se o event for ativado, quer dizer que temos algo
-            # no buffer para ser tratado
             with self._buffer_lock:
-                # Aqui vamos remover apenas uma mensagem, porem temos que tratar
-                # (possivel laço), para poder remover todas de dentro
                 msg = self._buffer.pop(0)
-                print(f'Mensagem Recebida: {msg}')
+                print(f'Removi do Buffer: {msg}')
 
-                # Agora limpamos o evento para poder reutiliza-lo
+                # Limpamos o evento para poder reutiliza-lo
                 self._buffer_event.clear()
+
+    # Iniciando as Threads
+    def run(self):
+        print('Threads iniciadas!\nEm 60 segundos encerrarei a execução!')
+        self._consumer_thread.start()
+        self._producer_thread.start()
 
     # Caso queira parar a execução (kkkkkkkkkk)
     def stop_with_rolling_stones(self):
@@ -61,7 +48,12 @@ class Tela:
         self._consumer_status = False
         self._producer_status = False
 
+        self._consumer_thread.join()
+        self._producer_thread.join()
+
 if __name__ == "__main__":
-    tela = Tela()
+    conceito = ProducerAndConsumer()
+    conceito.run()
+
     time.sleep(60)
-    tela.stop_with_rolling_stones()
+    conceito.stop_with_rolling_stones()
