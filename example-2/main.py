76e202d384f9d657d0abe5492b7ce5a21101fce5
Alessandro César Rosão <142243633+AlldDev@users.noreply.github.com>
2024-07-11 16:54:05 -0300

    Create main.py

---
+++ example-2/main.py
@@ -0,0 +1,19 @@
+import queue
+import threading
+import consumer
+import producer
+
+if __name__ == '__main__':
+    #fila = queue.Queue()
+    fila = []
+    fila_lock = threading.Lock()
+    fila_event = threading.Event()
+
+    cl = producer.Client_producer(fila, fila_lock, fila_event)
+    sv = consumer.Server_consumer(fila, fila_lock, fila_event)
+
+    tt_client = threading.Thread(target=cl.producer)
+    tt_server = threading.Thread(target=sv.consumer)
+
+    tt_client.start()
+    tt_server.start()
