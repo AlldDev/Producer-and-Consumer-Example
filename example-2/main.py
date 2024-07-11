56ceb8abf24f17b6fe6bb8009e578759ec2d5d7d
Alessandro César Rosão <142243633+AlldDev@users.noreply.github.com>
2024-07-11 18:07:05 -0300

    Update main.py

--- example-2/main.py
+++ example-2/main.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
     fila_lock = threading.Lock()
     fila_event = threading.Event()
 
-    cl = producer.Client_producer(fila, fila_lock, fila_event)
-    sv = consumer.Server_consumer(fila, fila_lock, fila_event)
+    cl = producer.Producer(fila, fila_lock, fila_event)
+    sv = consumer.Consumer(fila, fila_lock, fila_event)
 
     tt_client = threading.Thread(target=cl.producer)
     tt_server = threading.Thread(target=sv.consumer)
